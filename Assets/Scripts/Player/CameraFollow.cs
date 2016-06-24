a90f10aa5140556e953c06ffb42e693d037f900b
Kasper Røgen <kasperhangard@hotmail.com>
2016-06-25 00:27:29 +0200

    CameraFollow v 1.0  - Follows the player strictly on x & y axis.  - Runs in update, potential for optimization?

---
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+
+    //Goes on: Camera
+    //Needs: Reference to player gameobject
+
+    public GameObject Player;
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 playerPosition = Player.transform.position; //Get the player position
+        playerPosition.z -= 1; //offset the camera from the game
+        this.transform.position = playerPosition; //Set the new camera position
+
+	}
+}
