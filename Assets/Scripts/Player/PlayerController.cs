6ee13e6b38bb27a7643ecbf81e345622062b76b2
Kasper Røgen <kasperhangard@hotmail.com>
2016-06-24 21:17:27 +0200

    First iteration of PlayerController, sideways movement on A and D, Jump on Space.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("a"))
+            this.transform.Translate(Vector2.left * Time.deltaTime);
+        if (Input.GetKey("d"))
+            this.transform.Translate(Vector2.right * Time.deltaTime);
+        if (Input.GetKeyDown("space")) { 
+            this.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 2, ForceMode2D.Impulse);
+        }
+    }
+}
