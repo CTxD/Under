570ac0c3f380f62ebd48510bdb1faef4fb9c9e4e
Kasper Røgen <Kasperhangard@hotmail.com>
2016-06-26 01:55:59 +0200

    Mining script update and player png

--- Assets/Mining.cs
+++ Assets/Mining.cs
@@ -3,10 +3,16 @@ using System.Collections;
 
 public class Mining : MonoBehaviour {
 
+    //Y-Axis bug on animation
+
     public LayerMask toCast;
     private float t = 0.0f;
     private bool isMining = false;
+    private bool active = false;
     private Vector2 mousePositionToAnimate;
+    private Quaternion endPosition;
+    public int offset = 0;
+    int count = 0;
     // Use this for initialization
     void Start () {
 	
@@ -16,38 +22,53 @@ public class Mining : MonoBehaviour {
 	void Update () {
         if (Input.GetMouseButtonDown(0))
         {
-            Vector2 convertedMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 clickDirection = convertedMousePosition - (Vector2)this.transform.GetChild(0).position;
-            float angle = (Mathf.Atan2(clickDirection.y, clickDirection.x) * Mathf.Rad2Deg) - 50;
 
-            transform.GetChild(0).rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-            //transform.GetChild(0).rotation *= Quaternion.Euler(0, 0, 90);
-            Debug.DrawRay(this.transform.GetChild(0).position, clickDirection.normalized * 0.4f);
-               RaycastHit2D hit = Physics2D.Raycast(this.transform.GetChild(0).position, clickDirection.normalized, 0.4f, toCast);
-            if (hit)
-                Destroy(hit.transform.gameObject);
-            isMining = true;
-            mousePositionToAnimate = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            t = 0.0f;
+
+            if (!active) {
+                active = true;
+                isMining = true;
+                count = 0;
+                mousePositionToAnimate = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector2 convertedMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector2 clickDirection = convertedMousePosition - (Vector2)this.transform.GetChild(0).position;
+                float angle = (Mathf.Atan2(clickDirection.y, clickDirection.x) * Mathf.Rad2Deg);
+
+
+
+                transform.GetChild(0).rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+                Debug.DrawRay(this.transform.GetChild(0).position, clickDirection.normalized * 0.4f);
+                RaycastHit2D hit = Physics2D.Raycast(this.transform.GetChild(0).position, clickDirection.normalized, 0.4f, toCast);
+                if (hit)
+                    Destroy(hit.transform.gameObject);
+            }
+
+
+
+
         }
 
-        if (t < 1.0 && isMining)
+        if (isMining && count < 20)
         {
-            t += Time.deltaTime;
-            Mine();
+            StartCoroutine(Mine());
+            offset += 5;
         }
-        else
+        else {
+            active = false;
             isMining = false;
-
-	}
-
-    void Mine()
-    {
-        Vector2 clickDirection = mousePositionToAnimate - (Vector2)this.transform.GetChild(0).position;
-        float angle = (Mathf.Atan2(clickDirection.y, clickDirection.x) * Mathf.Rad2Deg) - 50;
-        transform.GetChild(0).rotation = Quaternion.Lerp(transform.GetChild(0).rotation, Quaternion.Euler(transform.GetChild(0).rotation.x, transform.GetChild(0).rotation.y, 90), Time.deltaTime * 6f);
-        
+            offset = 0;
+        }
     }
 
+
+    IEnumerator Mine()
+    {
+        
+        float angle = (Mathf.Atan2(mousePositionToAnimate.y, mousePositionToAnimate.x) * Mathf.Rad2Deg);
+        transform.GetChild(0).rotation = Quaternion.AngleAxis(angle - offset, Vector3.forward);
+        Debug.Log("VLA");
+        count += 1;
+        yield return new WaitForSeconds(0.5f);
+    }
 
 }
