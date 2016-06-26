f19c1188b5a6895f4f28c81edc13f4d11f09ea97
Kasper Røgen <kasperhangard@hotmail.com>
2016-06-26 16:51:06 +0200

    First look at multipart Blocks
    Mining script v2.0
     - animation fixed
    
    block properties v 1.0
     - added health stats

---
+++ Assets/Scripts/Player/Mining.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mining : MonoBehaviour {
+
+    private bool animationIsRunning = false;
+
+    public LayerMask toCast;	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0) && !animationIsRunning) {
+            float angle;
+            
+
+            Vector2 convertedMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 clickDirection = convertedMousePosition - (Vector2)this.transform.GetChild(0).position;
+            angle = (Mathf.Atan2(clickDirection.y, clickDirection.x) * Mathf.Rad2Deg);
+
+            transform.GetChild(0).rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            Debug.DrawRay(this.transform.GetChild(0).position, clickDirection.normalized * 0.4f);
+
+            RaycastHit2D hit = Physics2D.Raycast(this.transform.GetChild(0).position, clickDirection.normalized, 0.4f, toCast);
+            if (hit) {
+                if (hit.collider.gameObject.GetComponent<BlockProperties>().health <= 0) { 
+                    Destroy(hit.collider.gameObject);
+                }
+                else { 
+                    hit.collider.gameObject.GetComponent<BlockProperties>().health--;
+                    EdgeCollider2D EC2D = hit.collider.transform.GetComponent<EdgeCollider2D>();
+                    Vector2[] points = EC2D.points;
+                    Vector2 midWay = (points[0] - points[1]);
+                    Debug.DrawRay(transform.position, EC2D.points[0], Color.blue, 5);
+                    Debug.DrawRay(transform.position, points[1], Color.blue, 5);
+                    EdgeCollider2D temp = EC2D.gameObject.AddComponent<EdgeCollider2D>();
+                    temp.points = new Vector2[] {points[0], midWay };
+                    EdgeCollider2D temp2 = EC2D.gameObject.AddComponent<EdgeCollider2D>();
+                    temp.points = new Vector2[] { midWay, points[1] };
+                }
+            }
+            StartCoroutine(Mine());
+            
+
+        }
+    }
+
+    IEnumerator Mine() {
+        animationIsRunning = true;
+        yield return new WaitForSeconds(0.02f);
+        transform.GetChild(0).rotation *= Quaternion.Euler(0, 0, -10);
+        yield return new WaitForSeconds(0.02f);
+        transform.GetChild(0).rotation *= Quaternion.Euler(0, 0, -10);
+        yield return new WaitForSeconds(0.02f);
+        transform.GetChild(0).rotation *= Quaternion.Euler(0, 0, -10);
+        yield return new WaitForSeconds(0.02f);
+        transform.GetChild(0).rotation *= Quaternion.Euler(0, 0, -10);
+        yield return new WaitForSeconds(0.02f);
+        transform.GetChild(0).rotation *= Quaternion.Euler(0, 0, -10);
+        yield return new WaitForSeconds(0.02f);
+        transform.GetChild(0).rotation *= Quaternion.Euler(0, 0, -10);
+        yield return new WaitForSeconds(0.02f);
+        transform.GetChild(0).rotation *= Quaternion.Euler(0, 0, -10);
+        yield return new WaitForSeconds(0.02f);
+        transform.GetChild(0).rotation *= Quaternion.Euler(0, 0, -10);
+        yield return new WaitForSeconds(0.02f);
+        transform.GetChild(0).rotation *= Quaternion.Euler(0, 0, -10);
+        animationIsRunning = false;
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/World/BlockProperties.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockProperties : MonoBehaviour {
+
+    public short health = 5;
+    public static short[] colliderFull = new short[10];
+	// Use this for initialization1
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
